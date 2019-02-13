4d8fa866fd74542821ab9258349608732db89e93
eguigurens91 <eguigurenmariel@gmail.com>
2019-02-12 18:34:24 -0600

    Add Metodo POST

--- routes/api/Examen.js
+++ routes/api/Examen.js
@@ -31,6 +31,24 @@ router.get('/', function(req,res,next){
     }
 });
 
+router.post('/new',function(req,res,next){
+    var _Examendata = Object.assign({},Examendata,req.body);
+    _Examendata._id = uuidv4();
+    if(!data){
+        data =[];
+    }
+    data.push(_Examendata);
+    fileModel.write(data, function(err){
+        if(err){
+            console.log(err);
+            return res.status(500).json({'error':'Error en la DATA!'});
+        }
+        return res.status(200).json(_Examendata);
+    });
+});
+
+
+
 fileModel.read(function(err , filedata){
   if(err){
     console.log(err);
