88577607bf9a069b0fedfae6c4069bf2d4bcc109
eguigurens91 <eguigurenmariel@gmail.com>
2019-02-12 19:15:39 -0600

    Add metodo Delete

--- routes/api/Examen.js
+++ routes/api/Examen.js
@@ -15,6 +15,7 @@ var Examendata={
     'Telefono':''
     };
 
+//Metodo Get
 router.get('/', function(req,res,next){
     if(!data){
         fileModel.read(function(err,filedata){
@@ -31,6 +32,7 @@ router.get('/', function(req,res,next){
     }
 });
 
+//Metodo POST
 router.post('/new',function(req,res,next){
     var _Examendata = Object.assign({},Examendata,req.body);
     _Examendata._id = uuidv4();
@@ -47,9 +49,9 @@ router.post('/new',function(req,res,next){
     });
 });
 
-
+//Metodo Put
 router.put('/done/:ExamenId', function(req, res, next){
-    var _ExamenId = req.params.thingId;
+    var _ExamenId = req.params.ExamenId;
     var _ExamenUpds = req.body;
     var _ExamenUpdated = null;
     var newData = data.map(
@@ -75,6 +77,27 @@ router.put('/done/:ExamenId', function(req, res, next){
       return res.status(200).json(_ExamenUpdated);
     });
   });
+
+  //Metodo Delete
+  router.delete('/delete/:ExamenId', function(req, res, next){
+    var _ExamenId = req.params._ExamenId;
+    var newData = data.filter(
+      function (doc, i) {
+        if (doc._id == _ExamenId) {
+          return false;
+        }
+        return true;
+      }
+    );
+    data = newData;
+    fileModel.write(data, function (err) {
+      if (err) {
+        console.log(err);
+        return res.status(500).json({ 'error': 'Error al Guardar Data' });
+      }
+      return res.status(200).json({"delete": _ExamenId});
+    });
+  }); 
   
 fileModel.read(function(err , filedata){
   if(err){
@@ -83,3 +106,5 @@ fileModel.read(function(err , filedata){
     data = JSON.parse(filedata);
   }
 });
+
+module.exports = router;
